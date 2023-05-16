d2fe8612f0a17955701cd0a826fe1bac22096c64
Jérémie Drouet <jeremie.drouet@gmail.com>
2023-05-16 15:10:02 +0000

    init: create udp server
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use tokio::net::UdpSocket;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let sock = UdpSocket::bind("0.0.0.0:4545").await?;
+    let mut buffer = [0; 512];
+    loop {
+        let (len, addr) = sock.recv_from(&mut buffer).await?;
+        println!("{:?} bytes received from {:?}", len, addr);
+
+        let len = sock.send_to(&buffer[..len], addr).await?;
+        println!("{:?} bytes sent", len);
+    }
+}
