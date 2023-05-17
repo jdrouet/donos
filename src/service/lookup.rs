12d7526f29c47e4a297337acf7c845d8a97ab478
Jérémie Drouet <jeremie.drouet@gmail.com>
2023-05-17 19:01:36 +0000

    chore: generate lookup packet id
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- src/service/lookup.rs
+++ src/service/lookup.rs
@@ -1,11 +1,13 @@
 use crate::buffer::BytePacketBuffer;
 use crate::packet::{DnsPacket, DnsQuestion, QueryType};
 use std::io::Result;
+use std::sync::atomic::{AtomicU16, Ordering};
 use tokio::net::UdpSocket;
 
 pub struct LookupService {
     socket: UdpSocket,
     server: (&'static str, u16),
+    index: AtomicU16,
 }
 
 impl LookupService {
@@ -13,13 +15,17 @@ impl LookupService {
         let socket = UdpSocket::bind(("0.0.0.0", 43210)).await?;
         let server = ("1.1.1.1", 53);
 
-        Ok(Self { socket, server })
+        Ok(Self {
+            socket,
+            server,
+            index: AtomicU16::new(0),
+        })
     }
 
     pub async fn lookup(&self, qname: &str, qtype: QueryType) -> Result<DnsPacket> {
         let mut packet = DnsPacket::new();
 
-        packet.header.id = 6666;
+        packet.header.id = self.index.fetch_add(1, Ordering::SeqCst);
         packet.header.questions = 1;
         packet.header.recursion_desired = true;
         packet
